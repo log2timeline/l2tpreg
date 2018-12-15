96162c967e7f33c40001a91c403219baafd983ad
Joachim Metz <joachim.metz@gmail.com>
2018-12-15 16:09:07 +0100

    Added test_dependencies.ini and applied updates (#37)

--- utils/check_dependencies.py
+++ utils/check_dependencies.py
@@ -3,21 +3,23 @@
 """Script to check for the availability and version of dependencies."""
 
 from __future__ import print_function
+
 import sys
 
 # Change PYTHONPATH to include dependencies.
-sys.path.insert(0, u'.')
+sys.path.insert(0, '.')
 
 import utils.dependencies  # pylint: disable=wrong-import-position
 
 
-if __name__ == u'__main__':
+if __name__ == '__main__':
   dependency_helper = utils.dependencies.DependencyHelper()
 
   if not dependency_helper.CheckDependencies():
     build_instructions_url = (
-        u'https://github.com/log2timeline/plaso/wiki/Users-Guide')
+        'https://github.com/log2timeline/plaso/wiki/Users-Guide')
 
-    print(u'See: {0:s} on how to set up l2tpreg.'.format(
-        build_instructions_url))
-    print(u'')
+    print('See: {0:s} on how to set up l2tpreg.'.format(build_instructions_url))
+    print('')
+
+    sys.exit(1)
